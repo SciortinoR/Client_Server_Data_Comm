28a3bdfbe70696ac6245e86c5bbf2d0fb7784721
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-11-01 15:12:10 -0400

    Changed file structure, added README.md & .gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# Client Server Data Communication
+
+Simple Flask app employing data serialization/deserialization techniques for demonstrating communications for resources represented by data models.
+
+View Assignment1.pdf for more details.
---
+++ Source/server.py
@@ -0,0 +1,24 @@
+from flask_api import status
+from flask_restful import Resource, Api
+from flask import Flask, make_response, request
+
+from json import dumps, loads
+
+# Setup Flask server
+server = Flask(__name__)
+api = Api(server)
+
+class JSONResponseForData(Resource):
+    def post(self):
+        return 
+
+class ProtoBufResponseForData(Resource):
+    def post(self):
+        return
+
+# URL endpoints for JSON & ProtoBuf
+api.add_resource(JSONResponseForData, '/json_rfd')
+api.add_resource(ProtoBufResponseForData, '/protobuf_rfd')
+
+if __name__ == '__main__':
+    server.run()
